ca653a523621c1fa559cf91a9fab6585a8cd0b4f
Warren Dugan <warrendugan@gmail.com>
2022-03-30 19:17:53 -0700

    initial commit

--- apps/www/src/app/app.component.ts
+++ apps/www/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+interface RandomType {
+  test: string;
+}
 @Component({
-  selector: 'warrendugan-root',
+  selector: 'main',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'www';
+
+  public randomJunk: RandomType = { test: 'Warren Rocks' };
+  private _junk: unknown = {};
+  get junk(): string {
+    return JSON.stringify(this._junk);
+  }
+
+  set junk(value: string) {
+    this._junk = JSON.parse(value);
+  }
+
+  handleClick(event: MouseEvent) {
+      const result = event.currentTarget as HTMLButtonElement;
+      const test = result.attributes.getNamedItem('derp');
+      this.junk = test.nodeValue;
+      console.log(this.junk);
+      this.title = (this._junk as RandomType).test;
+  }
 }
